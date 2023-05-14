9c4bee82e0c5c59522e2ca35823f00cc78e4e097
Mattias Lucas <mathiaseluma@gmail.com>
2023-05-14 03:28:11 -0400

    new upload

---
+++ payscore/lib/Screens/recipient.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Recipient extends StatefulWidget {
+  const Recipient({super.key});
+
+  @override
+  State<Recipient> createState() => _RecipientState();
+}
+
+class _RecipientState extends State<Recipient> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
